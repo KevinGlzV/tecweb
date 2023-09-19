10132e016b4196964d380b5de59a3858bb89f07a
KevinGlzV <kevin.gonzalezv@alumno.buap.mx>
2023-09-19 15:22:45 -0600

    PRACTICA 05. INTRODUCCION A PHPMYADMIN

---
+++ practicas/p05/get_productos_xhtml.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <h3>PRODUCTOS</h3>
+
+    <br/>
+
+    <?php
+    if(isset($_GET['tope']))
+    {
+        $tope = $_GET['tope'];
+
+        /** SE CREA EL OBJETO DE CONEXIÓN */
+        @$link = new mysqli('localhost', 'root', '11223344', 'marketzone');
+
+        /** comprobar la conexión */
+        if ($link->connect_errno) 
+        {
+            die('Falló la conexión: '.$link->connect_error.'<br/>');
+            /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+        }
+
+        /** Crear una tabla que no devuelve un conjunto de resultados */
+        if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope")) 
+        {
+            ?>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Unidades</th>
+                        <th scope="col">Detalles</th>
+                        <th scope="col">Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($row = $result->fetch_array(MYSQLI_ASSOC)) 
+                    {
+                        ?>
+                        <tr>
+                            <th scope="row"><?= $row['id'] ?></th>
+                            <td><?= $row['nombre'] ?></td>
+                            <td><?= $row['marca'] ?></td>
+                            <td><?= $row['modelo'] ?></td>
+                            <td><?= $row['precio'] ?></td>
+                            <td><?= $row['unidades'] ?></td>
+                            <td><?= utf8_encode($row['detalles']) ?></td>
+                            <td><img src="<?= $row['imagen'] ?>"></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <?php
+            /** útil para liberar memoria asociada a un resultado con demasiada información */
+            $result->free();
+        }
+
+        $link->close();
+    }
+    else
+    {
+        echo 'Parámetro "tope" no detectado...';
+    }
+    ?>
+</body>
+</html>
